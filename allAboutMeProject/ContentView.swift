f4aac9dd8ef5abc48e81b19241b4fef7148ecfd7
Lauren <lravury>
2023-04-22 18:09:37 -0500

    Initial Commit

---
+++ allAboutMeProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  allAboutMeProject
+//
+//  Created by Lauren Ravury on 4/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ allAboutMeProject/allAboutMeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  allAboutMeProjectApp.swift
+//  allAboutMeProject
+//
+//  Created by Lauren Ravury on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct allAboutMeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
